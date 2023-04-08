9238ad5953efe3852adc64dfac1bb53b89a1fc65
kdkdhoho <hkim4410@naver.com>
2023-04-08 16:11:29 +0900

    refactor(CodeSendPractice): 코드 개선
    - Color Enum 객체 추가
    - 클래스 순서 조정

--- src/main/java/org/example/CodeSendPractice.java
+++ src/main/java/org/example/CodeSendPractice.java
@@ -1,43 +1,21 @@
 package org.example;
 
+import static org.example.Color.GREEN;
+import static org.example.Color.RED;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class CodeSendPractice {
-    public static void main(String[] args) {
-        // 1. Apple 리스트를 생성한다.
-        List<Apple> apples = new ArrayList<>(List.of(
-                new Apple(1, "red"),
-                new Apple(50, "green"),
-                new Apple(100, "red"),
-                new Apple(30, "red"),
-                new Apple(80, "green"))
-        );
-
-        // 2. Apple내에 static 메서드를 만들어 color가 red인 사과를 필터링한다.
-        List<Apple> redApples = Apple.filterRedApples(apples);
-        System.out.println(redApples); // [Apple{size=1, color='red'}, Apple{size=100, color='red'}, Apple{size=30, color='red'}]
-
-        // 3. Apple내에 static 메서드를 만들어 size가 50 이상인 사과를 필터링한다.
-        List<Apple> bigApples = Apple.filterBigApples(apples);
-        System.out.println(bigApples); // [Apple{size=50, color='green'}, Apple{size=100, color='red'}, Apple{size=80, color='green'}]
-
-        // 4. 메서드를 전달하는 방식으로 color가 red인 사과를 필터링한다.
-        List<Apple> advancedRedApples = Apple.filterApples(apples, apple -> apple.isRedApple(apple));
-        System.out.println(advancedRedApples); // [Apple{size=1, color='red'}, Apple{size=100, color='red'}, Apple{size=30, color='red'}]
-
-        // 5. 메서드를 전달하는 방식으로 size가 50 이상인 사과를 필터링한다.
-        List<Apple> advancedBigApples = Apple.filterApples(apples, apple -> apple.isBigApple(apple));
-        System.out.println(advancedBigApples); // [Apple{size=50, color='green'}, Apple{size=100, color='red'}, Apple{size=80, color='green'}]
-    }
+enum Color {
+    RED, GREEN
 }
 
 class Apple {
     private final int size;
-    private final String color;
+    private final Color color;
 
-    public Apple(final int size, final String color) {
+    public Apple(final int size, final Color color) {
         this.size = size;
         this.color = color;
     }
@@ -46,7 +24,7 @@ class Apple {
         List<Apple> result = new ArrayList<>();
 
         for (final Apple apple : apples) {
-            if (apple.color.equals("red")) {
+            if (apple.color.equals(RED)) {
                 result.add(apple);
             }
         }
@@ -76,7 +54,7 @@ class Apple {
     }
 
     public boolean isRedApple(final Apple apple) {
-        return apple.color.equals("red");
+        return apple.color.equals(RED);
     }
 
     public boolean isBigApple(final Apple apple) {
@@ -91,3 +69,34 @@ class Apple {
                 '}';
     }
 }
+
+
+public class CodeSendPractice {
+    public static void main(String[] args) {
+        // 1. Apple 리스트를 생성한다.
+        List<Apple> apples = new ArrayList<>(List.of(
+                new Apple(1, RED),
+                new Apple(50, GREEN),
+                new Apple(100, RED),
+                new Apple(30, RED),
+                new Apple(80, GREEN))
+        );
+
+        // 2. Apple내에 static 메서드를 만들어 color가 red인 사과를 필터링한다.
+        List<Apple> redApples = Apple.filterRedApples(apples);
+        System.out.println(redApples); // [Apple{size=1, color='red'}, Apple{size=100, color='red'}, Apple{size=30, color='red'}]
+
+        // 3. Apple내에 static 메서드를 만들어 size가 50 이상인 사과를 필터링한다.
+        List<Apple> bigApples = Apple.filterBigApples(apples);
+        System.out.println(bigApples); // [Apple{size=50, color='green'}, Apple{size=100, color='red'}, Apple{size=80, color='green'}]
+
+        // 4. 메서드를 전달하는 방식으로 color가 red인 사과를 필터링한다.
+        List<Apple> advancedRedApples = Apple.filterApples(apples, apple -> apple.isRedApple(apple));
+        System.out.println(advancedRedApples); // [Apple{size=1, color='red'}, Apple{size=100, color='red'}, Apple{size=30, color='red'}]
+
+        // 5. 메서드를 전달하는 방식으로 size가 50 이상인 사과를 필터링한다.
+        List<Apple> advancedBigApples = Apple.filterApples(apples, apple -> apple.isBigApple(apple));
+        System.out.println(advancedBigApples); // [Apple{size=50, color='green'}, Apple{size=100, color='red'}, Apple{size=80, color='green'}]
+    }
+}
+
